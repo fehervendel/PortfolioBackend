48628f8930a21c5f78329dde91437a78d0f7e794
fehervendel <fehervendel@gmail.com>
2025-04-01 13:58:40 +0200

    feature: add Content dbset to PortfolioContext.cs

--- Portfolio/Context/PortfolioContext.cs
+++ Portfolio/Context/PortfolioContext.cs
@@ -6,6 +6,7 @@ namespace Portfolio.Context;
 public class PortfolioContext : DbContext
 {
     public DbSet<Card> Cards { get; set; }
+    public DbSet<Content> Content { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
