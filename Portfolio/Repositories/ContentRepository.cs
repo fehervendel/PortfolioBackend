f5814b7dc665f2a118c50e317cbb89d44c80c958
fehervendel <fehervendel@gmail.com>
2025-04-01 14:03:56 +0200

    feature: add IContentRepository.cs and ContentRepository.cs for CRUD methods

---
+++ Portfolio/Repositories/ContentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Context;
+using Portfolio.Models;
+
+namespace Portfolio.Repositories;
+
+public class ContentRepository : IContentRepository
+{
+    public async Task<IEnumerable<Content>> GetContent()
+    {
+        using var dbContext = new PortfolioContext();
+        return await dbContext.Content.ToListAsync();
+    }
+
+    public async Task<Content> GetById(int id)
+    {
+        using var dbContext = new PortfolioContext();
+        return await dbContext.Content.FirstAsync(c => c.Id == id);
+    }
+
+    public async Task Add(Content content)
+    {
+        using var dbContext = new PortfolioContext();
+        dbContext.Add(content);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task EditContent(int id, string textContent, string sectionId, int order)
+    {
+        using var dbContext = new PortfolioContext();
+
+        Content contentToEdit = await dbContext.Content.FirstAsync(c => c.Id == id);
+
+        contentToEdit.TextContent = textContent;
+        contentToEdit.SectionId = sectionId;
+        contentToEdit.Order = order;
+
+        dbContext.Update(contentToEdit);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteById(int id)
+    {
+        using var dbContext = new PortfolioContext();
+        Content contentToDelete = await dbContext.Content.FirstAsync(c => c.Id == id);
+
+        dbContext.Remove(contentToDelete);
+        await dbContext.SaveChangesAsync();
+    }
+}
---
+++ Portfolio/Repositories/IContentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Portfolio.Models;
+
+namespace Portfolio.Repositories;
+
+public interface IContentRepository
+{
+    Task<IEnumerable<Content>> GetContent();
+    Task<Content> GetById(int id);
+    Task Add(Content content);
+    Task EditContent(int id, string textContent, string sectionId, int order);
+    Task DeleteById(int id);
+}
