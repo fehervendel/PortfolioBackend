c0844330cb14e6fd4a0811604008dcf576339179
fehervendel <fehervendel@gmail.com>
2025-04-04 10:09:11 +0200

    feature: add EmailRepository.cs + Interface

---
+++ Portfolio/Repositories/EmailRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Context;
+using Portfolio.Models;
+
+namespace Portfolio.Repositories;
+
+public class EmailRepository : IEmailRepository
+{
+    private readonly PortfolioContext _dbContext;
+
+    public EmailRepository(PortfolioContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Email>> GetEmails()
+    {
+        return await _dbContext.Emails.ToListAsync();
+    }
+
+    public async Task Add(Email email)
+    {
+        _dbContext.Add(email);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<Email> GetById(int id)
+    {
+        return await _dbContext.Emails.FirstAsync(c => c.Id == id);
+    }
+}
---
+++ Portfolio/Repositories/IEmailRepository.cs
@@ -0,0 +1,10 @@
+﻿using Portfolio.Models;
+
+namespace Portfolio.Repositories;
+
+public interface IEmailRepository
+{
+    Task<IEnumerable<Email>> GetEmails();
+    Task Add(Email email);
+    Task<Email> GetById(int id);
+}
