06294bf4e9bdf6cf3602c5386f927b6350f09847
fehervendel <fehervendel@gmail.com>
2025-04-04 10:08:54 +0200

    feature: add Email model

---
+++ Portfolio/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Portfolio.Models;
+
+public class Email
+{
+    [Key]
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string EmailAddress { get; set; }
+    [Column(TypeName = "nvarchar(MAX)")]
+    public string Message { get; set; }
+}
