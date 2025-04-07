b097cee4f17ef56c516a20ffacc6f8b297b91fa3
fehervendel <fehervendel@gmail.com>
2025-04-07 17:14:02 +0200

    feature: add ModelController.cs

---
+++ Portfolio/Controllers/ModelController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Context;
+
+namespace Portfolio.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ModelController : ControllerBase
+{
+    private readonly PortfolioContext _context;
+
+    public ModelController(PortfolioContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("GetModels")]
+    public IActionResult GetDbNames()
+    {
+        var dbSetNames = _context.Model.GetEntityTypes()
+            .Select(e => e.Name)
+            .ToList();
+
+        return Ok(dbSetNames);
+    }
+}
